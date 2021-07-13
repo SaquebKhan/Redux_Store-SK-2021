8e1fec53d307ed480c2a142443e6c6db94ed2f5e
SaquebKhan <82435053+SaquebKhan@users.noreply.github.com>
2021-07-13 13:12:00 -0400

    Update

---
+++ client/src/utils/store.js
@@ -0,0 +1,5 @@
+//TOD0:
+import { createStore } from 'redux';
+import reducers from './reducers';
+// Create a default export of `createStore` that accepts an argument of `reducers`.
+export default createStore(reducers);
